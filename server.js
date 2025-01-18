b531ef742009643e6a3a019517b6003275abe2a5
revathy <revathyramdas99@gmail.com>
2025-01-18 20:56:47 +0530

    Added dev Changes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/git-demo',(req,res)=>{
     return res.status(200).json({message:"git demo"})
 })
 
+app.get('/dev',(req,res)=>{
+    return res.status(200).json({message:'Created at dev branch'})
+})
+
 
 app.listen(6000,()=>{
     console.log("App is Running @localhost://6000");
